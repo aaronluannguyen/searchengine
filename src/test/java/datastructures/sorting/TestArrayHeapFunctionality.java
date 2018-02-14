30621b7720dff4a1180cf2846f02213241888595
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-02-13 21:20:32 -0800

    Finished my set of tests

--- src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
+++ src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
@@ -16,6 +16,16 @@ public class TestArrayHeapFunctionality extends BaseTest {
     protected <T extends Comparable<T>> IPriorityQueue<T> makeInstance() {
         return new ArrayHeap<>();
     }
+    
+    protected IPriorityQueue<Integer> makeBasic() {
+        IPriorityQueue<Integer> basic = this.makeInstance();
+        basic.insert(1);
+        basic.insert(2);
+        basic.insert(3);
+        basic.insert(4);
+        basic.insert(5);
+        return basic;
+    }
 
     @Test(timeout=SECOND)
     public void testBasicSize() {
@@ -43,91 +53,149 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND)
     public void testSingleRemove() {
-        
+        IPriorityQueue<Integer> test = this.makeBasic();
+        assertEquals(1, test.removeMin());
     }
     
     @Test(timeout=SECOND)
     public void testRemoveMany() {
+        IPriorityQueue<Integer> test = this.makeInstance();
+        for (int i = 0; i < 250; i++) {
+            test.insert(i);
+        }
         
+        for (int i = 0; i < 250; i++) {
+            assertEquals(i, test.removeMin());
+        }
     }
     
     @Test(timeout=SECOND)
     public void testRemoveFromHeapWithSizeOne() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testRemoveEqualElements() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testPeekMinEmptyContainerException() {
-        
+        IPriorityQueue<Integer> empty = this.makeInstance();
+        try {
+            empty.peekMin();
+            fail("Expected EmptyContainerException");
+        } catch (EmptyContainerException ex) {
+            // Do nothing: this is ok
+        }
     }
     
     @Test(timeout=SECOND)
     public void testPeekMinHeapSizeOne() {
-        
+        IPriorityQueue<Integer> test = this.makeInstance();
+        test.insert(5);
+        assertEquals(5, test.peekMin());
     }
     
     @Test(timeout=SECOND)
     public void testPeekMinMany() {
+        IPriorityQueue<Integer> test = this.makeInstance();
+        for (int i = 0; i < 250; i++) {
+            test.insert(i);
+        }
         
+        for (int i = 0; i < 250; i++) {
+            assertEquals(i, test.peekMin());
+            test.removeMin();
+        }
     }
     
     @Test(timeout=SECOND) 
     public void testPeekMinTie() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testInsertNullException() {
-        
+        IPriorityQueue<Integer> test = this.makeInstance();
+        try {
+            test.insert(null);
+            fail("Expected IllegalArgumentException");
+        } catch (IllegalArgumentException ex) {
+            // Do nothing: this is ok
+        }
     }
     
     @Test(timeout=SECOND) 
     public void testInsertToEmptyHeap() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testInsertNewMin() {
+        IPriorityQueue<Integer> test = this.makeBasic();
+        test.insert(0);
+        assertEquals(0, test.peekMin());
         
+        test.insert(-5);
+        assertEquals(-5, test.peekMin());
     }
     
     @Test(timeout=SECOND)
     public void testInsertMany() {
+        IPriorityQueue<Integer> test = this.makeInstance();
+        for (int i = 0; i < 250; i++) {
+            test.insert(i);
+        }
         
+        for (int i = 0; i < 250; i++) {
+            assertEquals(i, test.removeMin());
+        }
+    }
+    
+    @Test(timeout=SECOND)
+    public void testInsertManyNewMins() {
+        IPriorityQueue<Integer> test = this.makeInstance();
+        for (int i = 250; i >= 0; i--) {
+            test.insert(i);
+        }
+        
+        for (int i = 0; i <= 250; i++) {
+            assertEquals(i, test.removeMin());
+        }
     }
     
     @Test(timeout=SECOND)
     public void testInsertAnEqualElement() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testSizeZero() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testSizeOne() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND) 
     public void testSizeMany() {
-        
+        IPriorityQueue<Integer> test = this.makeInstance();
+        for (int i = 0; i < 250; i++) {
+            test.insert(i);
+            assertEquals(i + 1, test.size());
+        }
     }
     
     @Test(timeout=SECOND)
     public void testIsEmptyTrue() {
-        
+        // ANI
     }
     
     @Test(timeout=SECOND)
     public void testIsEmptyFalse() {
-        
+        // ANI
     }
 }
