c43a79b4b190ed7cb365dfa9a81f52381bd9344d
Anirudh Kumar Subramanyam <ask710@uw.edu>
2018-02-13 21:21:16 -0800

    Add a few tests

--- src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
+++ src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
@@ -1,8 +1,11 @@
 package datastructures.sorting;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import misc.BaseTest;
+import misc.exceptions.EmptyContainerException;
 import datastructures.concrete.ArrayHeap;
 import datastructures.interfaces.IPriorityQueue;
 import org.junit.Test;
@@ -13,6 +16,16 @@ import org.junit.Test;
 public class TestArrayHeapFunctionality extends BaseTest {
     protected <T extends Comparable<T>> IPriorityQueue<T> makeInstance() {
         return new ArrayHeap<>();
+    }
+    
+    protected IPriorityQueue<Integer> makeBasicHeap(){
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        heap.insert(1);
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        return heap;
     }
 
     @Test(timeout=SECOND)
@@ -25,12 +38,19 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND)
     public void testEmptyContainerException() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        try {
+            heap.removeMin();
+            fail("Expected EmptyContainerException");           
+        }catch(EmptyContainerException ex) {
+            //Do nothing: this is expected.
+        }
     }
     
     @Test(timeout=SECOND)
     public void testSingleRemove() {
-        
+        IPriorityQueue<Integer> heap = this.makeBasicHeap();
+        assertEquals(1, heap.removeMin());
     }
     
     @Test(timeout=SECOND)
@@ -40,17 +60,33 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND)
     public void testRemoveFromHeapWithSizeOne() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        heap.insert(1);
+        assertEquals(1, heap.removeMin());
     }
     
     @Test(timeout=SECOND)
     public void testRemoveEqualElements() {
+        IPriorityQueue<Integer> heap = this.makeBasicHeap();
+        heap.insert(1);
+        heap.insert(1);        
         
+        assertEquals(1, heap.removeMin());
+        assertEquals(1, heap.removeMin());
+        assertEquals(1, heap.removeMin());
+        assertEquals(2, heap.removeMin());
+                
     }
     
     @Test(timeout=SECOND)
     public void testPeekMinEmptyContainerException() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        try {
+            heap.peekMin();
+            fail("Expected EmptyContainerException");           
+        }catch(EmptyContainerException ex) {
+            //Do nothing: this is expected.
+        }
     }
     
     @Test(timeout=SECOND)
@@ -65,7 +101,11 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND) 
     public void testPeekMinTie() {
+        IPriorityQueue<Integer> heap = this.makeBasicHeap();
+        heap.insert(1);
+        heap.insert(1); 
         
+        assertEquals(1, heap.peekMin());
     }
     
     @Test(timeout=SECOND)
@@ -75,7 +115,10 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND) 
     public void testInsertToEmptyHeap() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        heap.insert(2);
+        assertEquals(2, heap.peekMin());
+        assertEquals(2, heap.removeMin());
     }
     
     @Test(timeout=SECOND)
@@ -85,17 +128,25 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND)
     public void testInsertAnEqualElement() {
-        
+        IPriorityQueue<Integer> heap = this.makeBasicHeap();
+        heap.insert(1);
+        heap.insert(1);
+        assertEquals(1, heap.removeMin());
+        assertEquals(1, heap.removeMin());
+        assertEquals(1, heap.removeMin());
     }
     
     @Test(timeout=SECOND)
     public void testSizeZero() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        assertEquals(0, heap.size());
     }
     
     @Test(timeout=SECOND)
     public void testSizeOne() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        heap.insert(1);
+        assertEquals(1, heap.size());
     }
     
     @Test(timeout=SECOND) 
@@ -105,11 +156,13 @@ public class TestArrayHeapFunctionality extends BaseTest {
     
     @Test(timeout=SECOND)
     public void testIsEmptyTrue() {
-        
+        IPriorityQueue<Integer> heap = this.makeInstance();
+        assertTrue(heap.isEmpty());
     }
     
     @Test(timeout=SECOND)
     public void testIsEmptyFalse() {
-        
+        IPriorityQueue<Integer> heap = this.makeBasicHeap();
+        assertFalse(heap.isEmpty());
     }
 }
