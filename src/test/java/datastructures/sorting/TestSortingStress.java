99428c7c63e02fc99197d79de1653235c6d47a8c
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-02-13 22:18:43 -0800

    Finished stress test for top k sort function

--- src/test/java/datastructures/sorting/TestSortingStress.java
+++ src/test/java/datastructures/sorting/TestSortingStress.java
@@ -1,9 +1,13 @@
 package datastructures.sorting;
 
 import misc.BaseTest;
+import misc.Searcher;
+
 import org.junit.Test;
 
 import datastructures.concrete.ArrayHeap;
+import datastructures.concrete.DoubleLinkedList;
+import datastructures.interfaces.IList;
 import datastructures.interfaces.IPriorityQueue;
 
 import static org.junit.Assert.assertFalse;
@@ -35,5 +39,18 @@ public class TestSortingStress extends BaseTest {
         }
         
         assertTrue(test.isEmpty());
+    }
+    
+    @Test(timeout=10*SECOND)
+    public void testStressForTopKSort() {
+        int limit = 100000;
+        IList<Integer> list = new DoubleLinkedList<>();
+        for(int i = 0; i < limit; i++) {
+            list.add(i);
+        }
+        IList<Integer> top = Searcher.topKSort(50000, list);
+        for(int i = 0 ; i < top.size(); i++) {
+            assertEquals(50000 + i, top.get(i));
+        }
     }
 }
