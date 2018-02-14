90a04cb986bc9b7cd072ad5ede1ab3ca0d566d83
Aaron Nguyen <aaronluannguyen@gmail.com>
2018-02-13 22:15:59 -0800

    Finished stress test for array heap

--- src/test/java/datastructures/sorting/TestSortingStress.java
+++ src/test/java/datastructures/sorting/TestSortingStress.java
@@ -3,15 +3,37 @@ package datastructures.sorting;
 import misc.BaseTest;
 import org.junit.Test;
 
+import datastructures.concrete.ArrayHeap;
+import datastructures.interfaces.IPriorityQueue;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
  * See spec for details on what kinds of tests this class should include.
  */
 public class TestSortingStress extends BaseTest {
+    protected <T extends Comparable<T>> IPriorityQueue<T> makeInstance() {
+        return new ArrayHeap<>();
+    }
+    
     @Test(timeout=10*SECOND)
-    public void testPlaceholder() {
-        // TODO: replace this placeholder with actual code
-        assertTrue(true);
+    public void testStressForArrayHeap() {
+        int limit = 100000;
+        IPriorityQueue<Integer> test = this.makeInstance();
+        
+        for (int i = limit; i >= 0; i++) {
+            test.insert(i);
+            assertEquals(i, test.peekMin());
+        }
+        
+        for (int i = 0; i <= limit; i++) {
+            assertEquals(i, test.peekMin());
+            assertEquals(i, test.removeMin());
+            assertEquals(limit - i, test.size());
+            assertFalse(test.isEmpty());
+        }
+        
+        assertTrue(test.isEmpty());
     }
 }
